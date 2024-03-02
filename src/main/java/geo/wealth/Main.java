f8c1de08ebc40fc8a7b39f18600d982bb296be89
m.ivanova <margaritawork76@gmail.com>
2024-03-02 11:09:32 +0200

    init commit

---
+++ src/main/java/geo/wealth/Main.java
@@ -0,0 +1,11 @@
+package geo.wealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class);
+    }
+}
