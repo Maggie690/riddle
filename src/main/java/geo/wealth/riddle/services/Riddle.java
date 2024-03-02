347d0875ace597893ec9e537ca43d24d12ddbc7a
m.ivanova <margaritawork76@gmail.com>
2024-03-02 20:14:28 +0200

    recursive algorithm to find sub words in the word

--- src/main/java/geo/wealth/riddle/services/Riddle.java
+++ src/main/java/geo/wealth/riddle/services/Riddle.java
@@ -10,7 +10,11 @@ import java.util.*;
 @RequiredArgsConstructor
 public class Riddle {
 
+    public static final String A = "A";
+    public static final String I = "I";
+    public static final String REGEX_LETTERS = "^[a-zA-Z]*$";
     private final Dictionary dictionary;
+
 
     public void findWords(String word) throws IOException {
 
@@ -37,4 +41,54 @@ public class Riddle {
         foundWords.forEach(System.out::println);
     }
 
+    public void findWordsByRecursion(String word) throws IOException {
+        if (word.isBlank()) {
+            //do something
+            return;
+        }
+
+        if (!word.matches(REGEX_LETTERS)) {
+            //do something
+            System.out.println("some exception");
+            return;
+        }
+
+        TreeMap<String, List<String>> wordsAlphabetic = dictionary.getDictionary();
+        Set<String> foundWords = new HashSet<>();
+
+        recursion(word, foundWords);
+        Set<String> realWords = checkWordExist(foundWords, wordsAlphabetic);
+
+        realWords.forEach(System.out::println);
+    }
+
+    private void recursion(String word, Set<String> words) {
+        if (!word.isEmpty()) {
+            words.add(word);
+
+            recursion(word.substring(1), words);
+            recursion(word.substring(0, word.length() - 1), words);
+        }
+    }
+
+    private Set<String> checkWordExist(Set<String> foundWords, TreeMap<String, List<String>> dictionary) {
+        var realWorlds = new TreeSet<String>();
+        for (var word : foundWords) {
+            List<String> rightWords = dictionary.get(String.valueOf(word.charAt(0)));
+            if (rightWords.contains(word)) {
+                realWorlds.add(word);
+            }
+        }
+
+        addExcludedLetterFromDictionary(foundWords, realWorlds, A);
+        addExcludedLetterFromDictionary(foundWords, realWorlds, I);
+
+        return realWorlds;
+    }
+
+    private static void addExcludedLetterFromDictionary(Set<String> foundWords, TreeSet<String> realWorlds, String letter) {
+        if (foundWords.contains(letter)) {
+            realWorlds.add(letter);
+        }
+    }
 }
